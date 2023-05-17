6650f9b348e2629069af80bc0027be9ba7487f14
carolinatatiani <carolinatatiani@gmail.com>
2023-05-16 21:55:03 -0300

    04

--- BookEntry.js
+++ BookEntry.js
@@ -7,9 +7,9 @@ function Submit() {
       console.log($BookName);
       $BookName = NewBook($BookName);
       console.log(BookName, $BookName);
-      BookList[0] = BookName;
+      BookList[0] = "<br> <a id='" + BookName + "'href='SelBook.html'>" + Bookname + " - " + $BookName[Author] + " </a> <br>;
       BookList.sort();
-      
+
 }
 
 function NewBook(array) {
@@ -25,4 +25,8 @@ function NewBook(array) {
 
 function Home() {
       window.open("Booklist.html")
+}
+
+function AllBooks() {
+      document.getElementById("BookList").innerHTML = BookList;
 }
