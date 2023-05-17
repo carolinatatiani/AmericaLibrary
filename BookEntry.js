65ec64706162892ae6b7e07c0c95f0ca79f5d45b
carolinatatiani <carolinatatiani@gmail.com>
2023-05-16 22:46:05 -0300

    end

--- BookEntry.js
+++ BookEntry.js
@@ -1,21 +1,27 @@
 var BookName
-var BookList
+let BookList = {};
+localStorage.clear();
+
 function Submit() {
       BookName = document.getElementById("Title").value;
       console.log(BookName);
       let $BookName = {};
       console.log($BookName);
       $BookName = NewBook($BookName);
-      console.log(BookName, $BookName);
-      BookList[0] = '<br> <a id="' + BookName + '" href="SelBook.html>"' + Bookname + ' - ' + $BookName[Author] + ' </a> <br>';
-      BookList.sort();
+      console.log($BookName["Author"]);
+      console.log(BookName)
+      BookList[0] = '<br><a id="' + BookName + '" href="SellBook.html">' + BookName + ' ' + $BookName["Author"] + '</a> <br>';
+      BookList.sort;
+      localStorage.setItem("BookList", BookList);
 
 }
 
 function NewBook(array) {
       let Book = [];
       Book['Title'] = document.getElementById("Title").value;
+      document.getElementById("Title").value = "";
       Book['Author'] = document.getElementById("Author").value;
+      document.getElementById("Author").value = "";
       Book['Age'] = document.getElementById("Age").value;
       Book['Category'] = document.getElementById("Category").value;
       Book['Language'] = document.getElementById("Language").value;
@@ -24,9 +30,14 @@ function NewBook(array) {
 }
 
 function Home() {
-      window.open("Booklist.html")
+
+      window.open("BookList.html");
+
+
+
 }
 
 function AllBooks() {
+
       document.getElementById("BookList").innerHTML = BookList;
 }
