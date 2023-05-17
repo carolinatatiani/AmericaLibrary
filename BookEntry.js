fd74cec2bf186ee3b9d0a353a9c31e8b38b0a1ed
carolinatatiani <carolinatatiani@gmail.com>
2023-05-16 21:28:49 -0300

    02

--- BookEntry.js
+++ BookEntry.js
@@ -1,1 +1,22 @@
+var BookName
+function Submit() {
+      BookName = document.getElementById("Title").value;
+      console.log(BookName);
+      let $BookName = {};
+      console.log($BookName);
+      $BookName = NewBook($BookName);
+      console.log(BookName, $BookName);
+    
+}
+
+function NewBook(array) {
+      let Book = [];
+      Book['Title'] = document.getElementById("Title").value;
+      Book['Author'] = document.getElementById("Author").value;
+      Book['Age'] = document.getElementById("Age").value;
+      Book['Category'] = document.getElementById("Category").value;
+      Book['Language'] = document.getElementById("Language").value;
+      Book['Section'] = document.getElementById("Section").value;
+      return Book
+}
 
