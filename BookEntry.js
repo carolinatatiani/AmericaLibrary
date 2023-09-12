d9e5aa492a50267c98c61b1aae8d8962212b62b1
carolinatatiani <carolinatatiani@gmail.com>
2023-09-12 09:04:25 -0300

    Aumento

--- BookEntry.js
+++ BookEntry.js
@@ -1,7 +1,106 @@
-var BookName
-let BookList = {};
-localStorage.clear();
+class Library {
+      constructor(Title, Author, Age, Category, Language, Section, Image) {
+            this.Title = Title;
+            this.Author = Author;
+            this.Age = Age;
+            this.Category = Category;
+            this.Language = Language;
+            this.Section = Section;
+      }
+}
 
+class Display {
+      add(bookoflibrary) {
+            console.log("Adding to UI");
+            let tableBody = document.getElementById('tableBody');
+            let uiString = `<tr>
+                            <td>${bookoflibrary.Title}</td>
+                            <td>${bookoflibrary.Author}</td>
+                            <td>${bookoflibrary.Age}</td>
+                            <td>${bookoflibrary.Category}</td>
+                            <td>${bookoflibrary.Language}</td>
+                            <td>${bookoflibrary.Section}</td>
+                        </tr>`;
+            tableBody.innerHTML += uiString;
+      }
+
+      clear() {
+            let libraryForm = document.getElementById('libraryForm');
+            libraryForm.reset();
+      }
+
+      validate(bookoflibrary) {
+            if (bookoflibrary.Title.length < 2 || bookoflibrary.Author.length < 2) {
+                  return false
+            }
+            else {
+                  return true;
+            }
+      }
+
+      show(type, displayMessage) {
+            let message = document.getElementById('message');
+            let boldText;
+            if (type === 'success') {
+                  boldText = 'Success';
+            }
+            else {
+                  boldText = 'Error!';
+            }
+            message.innerHTML = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+                                <strong>${boldText}:</strong> ${displayMessage}
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                                </button>
+                            </div>`;
+            setTimeout(function () {
+                  message.innerHTML = ''
+            }, 5000);
+
+      }
+}
+
+let libraryForm = document.getElementById('libraryForm');
+libraryForm.addEventListener('submit', libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+
+      console.log('You have submitted library form');
+
+      let Title = document.getElementById('Title').value;
+      let Author = document.getElementById('Author').value;
+      let Age = document.getElementById('Age').value;
+      let Category = document.getElementById('Category').value;
+      let Language = document.getElementById('Language').value;
+      let Section = document.getElementById('Section').value;
+
+
+
+      let bookoflibrary = new Library(Title, Author, Age, Category, Language, Section);
+      console.log(bookoflibrary);
+      localStorage.setItem("BookList", bookoflibrary);
+      window.open("BookList.html")
+      localStorage.getItem("BookList", bookoflibrary);
+      let display = new Display();
+
+      if (display.validate(bookoflibrary)) {
+
+            display.add(bookoflibrary);
+            display.clear();
+            display.show('success', 'Your book has been successfully added')
+      }
+      else {
+            // Show error to the user
+            display.show('danger', 'Sorry you cannot add this book');
+      }
+
+      e.preventDefault();
+}
+
+
+
+
+/*
 function Submit() {
       BookName = document.getElementById("Title").value;
       console.log(BookName);
@@ -17,27 +116,20 @@ function Submit() {
 }
 
 function NewBook(array) {
-      let Book = [];
-      Book['Title'] = document.getElementById("Title").value;
       document.getElementById("Title").value = "";
-      Book['Author'] = document.getElementById("Author").value;
       document.getElementById("Author").value = "";
-      Book['Age'] = document.getElementById("Age").value;
-      Book['Category'] = document.getElementById("Category").value;
-      Book['Language'] = document.getElementById("Language").value;
-      Book['Section'] = document.getElementById("Section").value;
-      return Book
+      document.getElementById("Age").value = "";
+      document.getElementById("Category").value = "";
+      document.getElementById("Language").value = "";
+      document.getElementById("Section").value = "";
+      document.getElementById("Image").value = "";
 }
 
 function Home() {
-
       window.open("BookList.html");
-
-
-
 }
 
 function AllBooks() {
-
       document.getElementById("BookList").innerHTML = BookList;
 }
+*/
